469b6fc663dc6ba02d0f02607642729f7c8a3dd8
ZeNullOrb <elyas1999sb@hotmail.com>
2023-01-22 10:43:44 +0300

    changing the out put of the functions

--- classes2.py
+++ classes2.py
@@ -41,14 +41,13 @@ class Vehicle:
     #--------------------------------------
 
     def drive(self):
-        return f"{self.__name} is driving!!"
+        return print(f"{self.__name} is driving!!")
 
     def drift(self):
-        return f"{self.__name} is drifting!!"
+        return print(f"{self.__name} is drifting!!")
 
     def carry_cargo(self):
-        return f"{self.__name} is carrying cargo!!"
-
+        return print(f"{self.__name} is carrying cargo!!")
 
 class Bus(Vehicle):
     
@@ -56,11 +55,11 @@ class Bus(Vehicle):
         super().__init__(brand, name, color, capacity, plate_number)
         self.wight = wight
 
-    def stop(self):
-        return f"{self.get_name()} is stoping!!"
+    def drift(self):
+        return print(f"{self.get_name()} is stoping!!")
 
-    def full(self):
-        return f"{self.get_name()} is Full!!"
+    def carry_cargo(self):
+        return print(f"{self.get_name()} is Full!!")
 
 class Truck(Vehicle):
     
@@ -68,24 +67,14 @@ class Truck(Vehicle):
         super().__init__(brand, name, color, capacity, plate_number)
         self.hight = hight
 
-    def load(self):
-        return f"{self.get_name()} is loading!!"
+    def drift(self):
+        return print(f"{self.get_name()} is loading!!")
 
 car = Vehicle("toyota","supra","white",2,"gta2030")
 
 bus = Bus("Mercedes-Benz","Daimler","silver",23,"sub0302",19.14)
-bus.set_brand("Mercedes-Benz")
-bus.set_name("Daimler")
-bus.set_color("silver")
-bus.set_capacity(23)
-bus.set_plate_number("sub0302")
 
 truck = Truck("Peterbilt","579EV","white",2,"tuk8050",80000)
-truck.set_brand("Peterbilt")
-truck.set_name("579EV")
-truck.set_color("white")
-truck.set_capacity(2)
-truck.set_plate_number("tuk8050")
 
 print(f"{car.get_brand()} \n{car.get_name()} \n{car.get_color()} \n{car.get_capacity()} \n{car.get_plate_number()}")
 
@@ -99,19 +88,19 @@ print(f"{truck.get_brand()} \n{truck.get_name()} \n{truck.get_color()} \n{truck.
 
 print("---"*10)
 
-print(car.drive())
-print(car.drift())
+car.drive()
+car.drift()
 
 print("---"*10)
 
-print(bus.drive())
-print(bus.stop())
-print(bus.full())
-print(bus.drive())
+bus.drive()
+bus.drift()
+bus.carry_cargo()
+bus.drive()
 
 print("---"*10)
 
-print(truck.drive())
-print(truck.load())
-print(truck.carry_cargo())
-print(truck.drive())
+truck.drive()
+truck.drift()
+truck.carry_cargo()
+truck.drive()
